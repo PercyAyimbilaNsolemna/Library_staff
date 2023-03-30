b9ad46fc050314d3f0db8da27e414bc7c50b0b9b
master <pnayimbila@st.ug.edu.gh>
2023-03-30 23:36:39 +0000

    Added attributes of the class.

--- LibraryStaff.py
+++ LibraryStaff.py
@@ -1,4 +1,16 @@
 class LibraryStaff:
     def __init__(self, full_name=None, year_of_employment=None, staffID=None, rank=None, gender=None, 
                     department=None, salary=None, year_of_birth=None, sanctions=None, city=None, region=None):
+                    self.full_name = full_name
+                    self.year_of_employment = year_of_employment
+                    self.staffID = staff_ID
+                    self.rank = rank
+                    self.gender = gender
+                    self.department = department
+                    self.salary = salary
+                    self.year_of_birth = year_of_birth
+                    self.sanctions = sanctions
+                    self.city = city
+                    self.region = region
+
                     
