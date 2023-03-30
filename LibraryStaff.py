2e6d94c09faf9f5440788c0bfc1a7dfc2beef10b
master <pnayimbila@st.ug.edu.gh>
2023-03-30 23:33:51 +0000

    Addeda __init__ method.

--- LibraryStaff.py
+++ LibraryStaff.py
@@ -1,2 +1,4 @@
 class LibraryStaff:
-    
+    def __init__(self, full_name=None, year_of_employment=None, staffID=None, rank=None, gender=None, 
+                    department=None, salary=None, year_of_birth=None, sanctions=None, city=None, region=None):
+                    
